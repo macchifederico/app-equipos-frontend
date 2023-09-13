71a55bacbdfc1fa07818b9d6f596e9cd8d3d1428
Federico Macchi <federico.macchi@gmail.com>
2023-09-12 21:07:26 -0300

    Selecciona, arma y muestra equipos y jugadores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { EquiposComponent } from './components/equipos/equipos.component';
 import { PlantelComponent } from './components/plantel/plantel.component';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { TarjetaComponent } from './components/tarjeta/tarjeta.component';
+import { CrearJugadorComponent } from './components/crear-jugador/crear-jugador.component';
 
 const routes: Routes = [
   {path:'', redirectTo: 'principal', pathMatch: 'full'},
   {path: 'principal', component: PrincipalComponent},
   {path: 'equipos', component: EquiposComponent},
   {path: 'plantel', component: PlantelComponent},
-  {path: 'jugadores', component: TarjetaComponent}
+  {path: 'jugadores', component: TarjetaComponent},
+  {path: 'crear_jugador', component: CrearJugadorComponent}
 
 ];
 
